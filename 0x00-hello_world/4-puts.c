f66c1a6c2d5c75fb99cd73ec9b14192b4957d0d1
MercyKorir <mcthegreat78@gmail.com>
2022-06-02 05:22:02 -0700

    I have added the file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
 * main - Entry point
 *
@@ -8,7 +9,7 @@
 int main(void)
 {
 	char str1[51];
-	strcpy(str1, "Programming is like building a multilingual puzzle");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 	puts(str1);
 	return (0);
 }
