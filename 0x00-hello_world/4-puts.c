caeb5aed0397389ed6ff188e0143e19db94b4ea6
MercyKorir <mcthegreat78@gmail.com>
2022-06-02 04:55:19 -0700

    I have added the file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char str1[51]
+	strcpy(str1, "Programming is like building a multilingual puzzle");
+	puts(str1);
+	return (0);
+}
