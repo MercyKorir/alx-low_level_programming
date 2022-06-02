a766743b16cea870a6256361416068be55a49fc3
MercyKorir <mcthegreat78@gmail.com>
2022-06-02 07:47:25 -0700

    I have added the file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main -Entry point
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)");
+	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 }
