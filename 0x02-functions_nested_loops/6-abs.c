481f67edff693d8a15f0f3c7b19c56e05c63c68b
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 15:32:44 -0700

    I have added the file 6*

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,8 +5,8 @@
 /**
 * _abs - prints the absolute value for an integer
 * @c: integer to be operated on
-* Return: the value of the absolute value.
-*/ 
+* Return: the absolute value is printed.
+*/
 int _abs(int c)
 {
 	int abs_c = abs(c);
