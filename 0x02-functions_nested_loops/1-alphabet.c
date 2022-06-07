1d3d16b1ef594b76d0642ebd7cac6b3f6f11fc59
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 11:17:36 -0700

    I hcae added the file 1*

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - this program prints alphabets in lowercase
- * void parameter 
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+* print_alphabet - this program prints alphabets in lowercase
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
 
 void print_alphabet(void)
