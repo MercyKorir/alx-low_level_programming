21f70a10939e50c7542ea43057e676d5fd0f9595
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 12:02:03 -0700

    I have added the file 3*

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* _islower - checks whether a character is lowercase or not
+* @c: Number to pass to _islower function
+*
+* Return: 1 if lowercase.
+* Return: 0 is otherwise.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _islower(int c)
+{
+	int r;
+
+	r = islower(c);
+
+	if (r)
+	{
+		write(1, "1", 1);
+	} else
+	{
+		_putchar(r + '0');
+	}
+	_putchar('\n');
+}
