8aea5bd084564ea162e52d149347822994202718
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 15:27:12 -0700

    I have added the file 6*

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _abs - prints the absolute value for an integer
+* @c: integer to be operated on
+* Return: the value of the absolute value.
+*/ 
+int _abs(int c)
+{
+	int abs_c = abs(c);
+	return (abs_c);
+}
