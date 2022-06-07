eed32af59d802e0b01e331e78606733a8116cc97
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 08:51:37 -0700

    Ihave added the file 1*

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-/*#include <unistd.h>*/
+#include <unistd.h>
 #include <ctype.h>
 
 /**
@@ -8,11 +8,11 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */ 
-/*int _putchar(char c)
-*{
-*	return (write(1, &c, 1));
-*}
-*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void print_alphabet(void)
 {
 	int lower_x;
