4c6a757dfbcf42baef7d83c9ae568b199a70a277
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 10:48:04 -0700

    I have added the file 1*

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+void print_alphabet(void)
+{
+	int lower_x;
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		lower_x = tolower(x);
+
+		putchar(lower_x);
+	}
+	putchar('\n');
+}
+
