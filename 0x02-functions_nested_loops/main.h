ef3547ad57d697de5451cf1ad4a33af725eb5745
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 09:55:31 -0700

    I have added the file 0*

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #include <unistd.h>
-/*#include <ctype.h>*/
 
 /**
 * _putchar - writes the character c to stdout
@@ -7,23 +6,8 @@
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
-*/ 
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/*void print_alphabet(void)
-*{
-*	int lower_x;
-*	int x;
-*
-*	for (x = 'A'; x <= 'Z'; x++)
-*	{
-*		lower_x = tolower(x);
-*
-*		_putchar(lower_x);
-*	}
-*	_putchar('\n');
-*}
-*/
