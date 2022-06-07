0718c55dfcc2dd4c1ac516fd0e152018a31d57f8
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 01:56:31 -0700

    I have added the file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,73 @@
 *
 * Return: Always 0 (Success)
 */
+
+int _putchar(char c);
+
 int main(void)
 {
-	write(1, "_putchar\n", 9);
+	int i;
 
+	for (i = 1; i <= 8; i++)
+	{
+		char c;
+
+		if (i == 1)
+		{
+			c = '_';
+
+			_putchar(c);
+		} else if (i == 2)
+		{
+			c = 'p';
+
+			_putchar(c);
+		} else if (i == 3)
+		{
+			c = 'u';
+
+			_putchar(c);
+		} else if (i == 4)
+		{
+			c = 't';
+
+			_putchar(c);
+		} else if (i == 5)
+		{
+			c = 'c';
+
+			_putchar(c);
+		} else if (i == 6)
+		{
+			c = 'h';
+
+			_putchar(0);
+		} else if (i == 7)
+		{
+			c = 'a';
+
+			_putchar(c);
+		} else if (i == 8)
+		{
+			c = 'r';
+
+			_putchar(c);
+		} else
+		{
+		}
+	}
+	write(1, "\n", 1);
 	return (0);
+}
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
