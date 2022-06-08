d52a8ae4e169faf4423f1d87ccfda087424d6d69
MercyKorir <mcthegreat78@gmail.com>
2022-06-07 17:02:40 -0700

    I have added the file 10*

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - adds two numbers and outputs the result
+* @a: The argument to be operated on
+* @b: The second argument to be operated on
+* Return: outputs the sum of the two numbers.
+*/
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
