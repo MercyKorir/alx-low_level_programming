7f7ef5dbb1fc5de2c0f7cce730096cdbc0dac481
MercyKorir <mcthegreat78@gmail.com>
2022-06-10 12:31:52 -0700

    I have added the file 5*

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers - this return numbers from 1 to 14 ten times
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j%10 + '0');
+		}
+		_putchar('\n');
+	}
+}
