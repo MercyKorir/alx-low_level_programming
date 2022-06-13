f5a0b3d71912361e7d4e462e1e236d694322d623
MercyKorir <mcthegreat78@gmail.com>
2022-06-13 01:50:39 -0700

    I have added the file 3*

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - prints a string to stdout
+* @str: this is the string to be printed
+* Return: void
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
