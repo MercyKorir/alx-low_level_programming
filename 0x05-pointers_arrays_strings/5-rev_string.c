634879dcebcc4432285bf4c924564997b12eaddc
MercyKorir <mcthegreat78@gmail.com>
2022-06-13 02:42:12 -0700

    I have added the file 6*

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: string to be reversed
+* Return: void.
+*/
+void rev_string(char *s)
+{
+	strrev(s);
+}
