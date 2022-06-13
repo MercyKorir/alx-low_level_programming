5e1ac6b343ca30598d49ddd64e89becfcd1a28e4
MercyKorir <mcthegreat78@gmail.com>
2022-06-13 06:01:59 -0700

    I have added the file 7*

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* puts_half - prints the last half of a string
+* @str: string to be printed
+* Return: void
+*/
+void puts_half(char *str)
+{
+	int len;
+	int len1;
+	int i;
+	int n;
+
+	len = strlen(str);
+	len1 = len / 2;
+
+	if (len % 2 == 0)
+	{
+		for (i = len1; i <= len; i++)
+		{
+			printf("%c", str[i]);
+		}
+	} else if (!(len % 2 == 0))
+	{
+		n = (len - 1) / 2;
+
+		for (i = (n + 1); i <= len; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+	printf("\n");
+}
