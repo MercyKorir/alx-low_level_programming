d277c96f8e6b6fddba3e1ad820c2f7c96269ed96
MercyKorir <mcthegreat78@gmail.com>
2022-06-13 07:21:26 -0700

    I have added the file 100*

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
-* int _atoi - converts a string to an integer
+* _atoi - converts a string to an integer
 * @s: string to be converted
 * Return: the integer is returned.
 */
