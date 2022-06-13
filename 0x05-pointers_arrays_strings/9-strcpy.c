ac42475a859732ae79cd576711c3bd2357d36641
MercyKorir <mcthegreat78@gmail.com>
2022-06-13 06:55:00 -0700

    I have added the file 9*

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcpy - copies text from source to destination
+* @dest: pointer where the text will be copied to
+* @src: source where text will be copied from
+* Return: the pointer to dest.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
