ccbe30448dd026ccec9fffdc77ebc5c456f79816
MercyKorir <mcthegreat78@gmail.com>
2022-06-13 02:32:04 -0700

    I have added 4*

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_rev - returns a string in reverse order
+* @s: the string to be operated on
+* Return: void.
+*/
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
