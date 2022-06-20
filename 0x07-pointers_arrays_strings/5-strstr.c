f01c6d9ec3447fb983ab234291003fd973dbc848
MercyKorir <mcthegreat78@gmail.com>
2022-06-20 12:44:26 -0700

    I have added the file 5*

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strstr - function locates a substring
+* @haystack: string to be scanned
+* @needle: character to be found
+* Return: the string
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *str;
+
+	str = strstr(haystack, needle);
+
+	if (str)
+	{
+		return (str);
+	}
+	else
+		return (NULL);
+}
