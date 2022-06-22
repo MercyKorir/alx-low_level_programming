ecf3f69e36b96bf23e8bd01710384a910a8e6a65
MercyKorir <mcthegreat78@gmail.com>
2022-06-22 05:37:22 -0700

    I have added the file 4*'

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,15 +5,17 @@
 * _pow_recursion - does power of two integers
 * @x: base integer
 * @y: raised to integer
-* Return: power or -1 if 
+* Return: power or -1 if
 * less than 0
 */
 int _pow_recursion(int x, int y)
 {
-	if (y >= 0)
+	if (y < 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
-	}
-	else
 		return (-1);
+	}
+	else if (y == 0)
+		return (1);
+	else
+		return (x * (_pow_recursion(x, (y - 1))));
 }
