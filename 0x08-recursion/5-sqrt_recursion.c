bec871464d4c18bee840bd6cda5a28664ef1284e
MercyKorir <mcthegreat78@gmail.com>
2022-06-22 05:58:55 -0700

    I have added the file 5*

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+* _sqrt_recursion - computes square root of a number
+* @n: number to be computed
+* Return: returns squareroot
+*/
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else
+		return (sqrt(n));
+}
