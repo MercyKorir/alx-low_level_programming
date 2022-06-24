f601e4d39d44bd1de9b4d26f035c428c61a3c187
MercyKorir <mcthegreat78@gmail.com>
2022-06-24 10:31:17 -0700

    I have added the file 0*

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - is the main function of program
+* @argc: number of arguments passed
+* @argv: pointer array which points to each argument
+* Return: 0 when successful
+*/
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	printf("\n");
+
+	return (0);
+}
