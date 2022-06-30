8abe9e2c7065282420d01805e92d9c839925bf31
MercyKorir <mcthegreat78@gmail.com>
2022-06-30 03:24:07 -0700

    I have added the file 1*

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strdup - returns a pointer to an allocated
+* space in memory
+* @str: string to be copied
+* Return: returns the pointer
+*/
+char *_strdup(char *str)
+{
+	char *copystr;
+
+	copystr = malloc(sizeof(str));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		copystr = strdup(str);
+
+		return (copystr);
+	}
+}
