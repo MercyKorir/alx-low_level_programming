78d3dcf45681750a0ef305dd4c11be57bcec826e
MercyKorir <mcthegreat78@gmail.com>
2022-07-04 11:37:13 -0700

    I have added the file dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+* struct dog - holds info about dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+* Description: Longer description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
