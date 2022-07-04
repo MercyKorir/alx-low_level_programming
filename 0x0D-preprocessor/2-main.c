fdf8a904b73732df03feb1ef8aec1743b02bd6a0
MercyKorir <mcthegreat78@gmail.com>
2022-07-04 10:27:27 -0700

    I have added the file 2*

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+int main(void)
+{
+	_putchar(__FILE__);
+	_putchar('\n');
+
+	return (0);
+}
