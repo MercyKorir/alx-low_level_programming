79b4eacddfee723a05a0a4021070cdd2969b2f60
MercyKorir <mcthegreat78@gmail.com>
2022-07-08 03:29:23 -0700

    I have added the file 1*

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 	{
+		return;
 	}
 
 	for (i = 0; i < size; i++)
