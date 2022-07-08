cf735eeb985f2e0bf8c53bceb32071743af9ab40
MercyKorir <mcthegreat78@gmail.com>
2022-07-08 02:57:34 -0700

    I have added the file 1*

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stddef.h>
 #include "function_pointers.h"
 
 /**
-* array_iterator - function that operates on elements
-* @array: array to be operated on
+* array_iterator - function iterates an array
+* @array: array to be iterated
 * @size: size of array
 * @action: callback function
 * Return: void
@@ -12,6 +11,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
+
+	if (action == NULL)
+	{
+	}
 
 	for (i = 0; i < size; i++)
 	{
