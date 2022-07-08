22c28838f07a3802a5b8da14444000b187c0fd40
MercyKorir <mcthegreat78@gmail.com>
2022-07-08 02:03:40 -0700

    I have added 0*

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,13 @@
 /**
 * print_name - function prints name
 * @name: value to be printed
-* @(*f)(char *): callback function
+* @f: callback function
 * Return: void
 */
 void print_name(char *name, void (*f)(char *))
 {
+	if (*f == NULL)
+	{
+	}
 	(*f)(name);
 }
