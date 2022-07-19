6e7be02bc85cf1c89c88b0afdae066a11314ddb0
MercyKorir <mcthegreat78@gmail.com>
2022-07-19 07:19:44 -0700

    added 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees up link
+* @head: pointer
+* Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+	
+	current = *head;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
