e65f1e6927a9ebcb5323092dd4abc8a7945a484d
MercyKorir <mcthegreat78@gmail.com>
2022-07-19 04:54:39 -0700

    mod 2*

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,6 +12,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (new_node == NULL)
 	{
+		return (NULL);
+	}
+	else
+	{
 		new_node->n = n;
 		new_node->next = (*head);
 		(*head) = new_node;
