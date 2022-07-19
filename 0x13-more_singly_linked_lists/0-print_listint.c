f7ebcedadab50446f92753ca1aeffbc7f62d7e66
MercyKorir <mcthegreat78@gmail.com>
2022-07-19 04:31:05 -0700

    mod 0*

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
 * print_listint - prints elements of a list
 * @h: pointer
-* Return: returns the number of nodes
+* Return: the number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
