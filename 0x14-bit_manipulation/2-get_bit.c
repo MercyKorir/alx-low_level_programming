a4d74b1acc1795179066b40e15e013f1f9f081ee
MercyKorir <mcthegreat78@gmail.com>
2022-07-21 06:27:40 -0700

    Added 2*

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* get_bit - gets value of bit at specific pos
+* @n: value to be checked
+* @index: position
+* Return: the value
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
