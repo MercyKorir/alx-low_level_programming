03e6c87cedb27144c60c3ddef786c5c279117006
MercyKorir <mcthegreat78@gmail.com>
2022-09-28 02:57:49 -0700

    I have added the file 0*

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,11 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
-		return NULL;
+		return (NULL);
 	hash->size = size;
 	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash->array == NULL)
-		return NULL;
+		return (NULL);
 	for (n = 0; n < size; n++)
 		hash->array[n] = NULL;
 	return (hash);
