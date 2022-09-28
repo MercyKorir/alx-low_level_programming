f430d45cbadc5859a65af26b331f14c84b04afd7
MercyKorir <mcthegreat78@gmail.com>
2022-09-28 09:52:37 -0700

    I have added the file 4*

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (i >= ht->size)
 		return (NULL);
 	curr_node = ht->array[i];
-	while (node && strcmp(node->key, key) != 0)
+	while (curr_node && strcmp(curr_node->key, key) != 0)
 		curr_node = curr_node->next;
 	return ((curr_node == NULL) ? NULL : curr_node->value);
 }
