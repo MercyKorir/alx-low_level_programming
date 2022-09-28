3e73297f161db7f7619e8ecc3e7fcb55a82cb13d
MercyKorir <mcthegreat78@gmail.com>
2022-09-28 04:00:34 -0700

    I have added the file 1*

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - This function implements the djb2 algorithm
+ * @str: string used to generate hash value
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str))
+	{
+		if (isupper(c))
+		{
+			c = c + 32;
+		}
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash % N);
+}
