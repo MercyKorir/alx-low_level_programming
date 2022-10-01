f84f56e9692fee23752a81811f7012540b7c1750
MercyKorir <mcthegreat78@gmail.com>
2022-10-01 05:24:03 -0700

    I have added the file 3*

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -7,11 +7,11 @@ int main(void)
 	dlistint_t *head;
 
 	head = NULL;
-	add_dnodeint(&head, 0);
-	add_dnodeint(&head, 1);
-	add_dnodeint(&head, 2);
-	add_dnodeint(&head, 3);
-	add_dnodeint(&head, 4);
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
 	print_dlistint(head);
 	return (EXIT_SUCCESS);
 }
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function adds a new node at end if
+ * a doubly linked list
+ * @head: head of current node
+ * @n: data of node to be added
+ * Return: address of new element
+ * NULL on failure
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	temp = *head;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (NULL);
+	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
+	new_node->prev = temp;
+	return (new_node);
+}
